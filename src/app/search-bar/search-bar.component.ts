040b86ba6d562c2b77a255f305ecb086e99d68f0
jasusetala <jasu.setala@metropolia.fi>
2017-03-07 18:13:03 +0200

    Haku toimii
    Search-bar haku toimii

--- src/app/search-bar/search-bar.component.ts
+++ src/app/search-bar/search-bar.component.ts
@@ -1,3 +1,5 @@
+import { Http, Response } from '@angular/http';
+import 'rxjs/add/operator/map';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -7,9 +9,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SearchBarComponent implements OnInit {
 
-  constructor() { }
+  private title: any = '';
+  private result: any = [];
+
+  constructor(private http: Http) { }
+
+   searchMovie = (formValues: any) => {
+    this.http.get('http://www.omdbapi.com/?t=' + formValues.title)
+    .subscribe(
+      (res: Response) => {
+        const json = res.json();
+        this.result = json;
+        console.log(this.result);
+      });
+      if (this.result.Response === 'True') {
+        document.getElementById('search-result').style.display = 'block';
+      } else {
+        console.log('ERROR');
+      }
+   }
 
   ngOnInit() {
   }
-
 }
