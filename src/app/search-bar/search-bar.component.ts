208e02ce0caff0a2ec8a9328fe46474f0ca6627c
jasusetala <jasu.setala@metropolia.fi>
2017-03-08 11:03:31 +0200

    Tallennus local strorageen
    Tallennus local strorageen hausta toimii.

--- src/app/search-bar/search-bar.component.ts
+++ src/app/search-bar/search-bar.component.ts
@@ -1,3 +1,4 @@
+import { WatchlistComponent } from './../watchlist/watchlist.component';
 import { Http, Response } from '@angular/http';
 import 'rxjs/add/operator/map';
 import { Component, OnInit } from '@angular/core';
@@ -11,6 +12,10 @@ export class SearchBarComponent implements OnInit {
 
   private title: any = '';
   private result: any = [];
+  private movieID: any = '';
+
+  private recentMovies: Array<string> = [];
+  private watchList: Array<string> = [];
 
   constructor(private http: Http) { }
 
@@ -22,10 +27,42 @@ export class SearchBarComponent implements OnInit {
         this.result = json;
         console.log(this.result);
       });
+      // Näytetään hakutulos vain jos Response === True
       if (this.result.Response === 'True') {
         document.getElementById('search-result').style.display = 'block';
       } else {
         console.log('ERROR');
+      }
+      // Otetaan hakutuloksen ID talteen muuttujaan, jotta se voidaan tarvittaessa lisätä local storageen
+      this.movieID = this.result.imdbID;
+      return this.movieID;
+   }
+
+   addToListRM() {
+     console.log('nappia painettu ja ID on ' + this.movieID);
+      if (localStorage.getItem('recentMovies') === null) {
+        this.recentMovies.push(this.movieID);
+        localStorage.setItem('recentMovies', JSON.stringify(this.recentMovies));
+        console.log(localStorage.getItem('recentMovies'));
+      } else {
+        this.recentMovies = JSON.parse(localStorage.getItem('recentMovies'));
+        this.recentMovies.push(this.movieID);
+        localStorage.setItem('recentMovies', JSON.stringify(this.recentMovies));
+        console.log(localStorage.getItem('recentMovies'));
+      }
+   }
+
+   addToListWL() {
+     console.log('nappia painettu ja ID on ' + this.movieID);
+      if (localStorage.getItem('watchList') === null) {
+        this.watchList.push(this.movieID);
+        localStorage.setItem('watchList', JSON.stringify(this.watchList));
+        console.log(localStorage.getItem('watchList'));
+      } else {
+        this.watchList = JSON.parse(localStorage.getItem('watchList'));
+        this.watchList.push(this.movieID);
+        localStorage.setItem('watchList', JSON.stringify(this.watchList));
+        console.log(localStorage.getItem('watchList'));
       }
    }
 
